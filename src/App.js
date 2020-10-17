d3c1e1456c6b98f806f50c52db42c0cb8dff6706
Joeway <joewaybanh@hotmail.com>
2020-10-17 14:45:15 +0200

    added states to click on to change online/offline status

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Contact from './components/Contact';
+// import Contact from './components/Contact';
 import ContactList from './components/ContactList'
  
 function App() {
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,29 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './Contact.css'
 import PropTypes from 'prop-types'
 
 
-const Contact = (props) => {
-    return (
-        <div className="Contact">
-            <img className="avatar" src={props.avatar} alt={props.name} />
-            <div className="name">
-                {props.name}
-                <div className="status">
-                    <div className={props.online ? "status-online" : "status-offline"}></div>
-                    <div className="status-text">
-                        {props.online?"online":"offline"}
+class Contact extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            online: false
+        }
+    }
+
+    checkOnline = () => {
+        const onOff = !this.state.online;
+        this.setState({ online: onOff })
+        console.log("sdad")
+    }
+
+    render() {
+        return (
+            <div className="Contact">
+                <img className="avatar" src={this.props.avatar} alt={this.props.name} />
+                <div className="name">
+                    {this.props.name}
+                    <div className="status">
+                        <div className={this.state.online ? "status-online" : "status-offline"}></div>
+                        <div onClick={() => this.checkOnline()} className="status-text">
+                            {this.state.online ? "online" : "offline"}
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
-
 
 Contact.propTypes = {
     name: PropTypes.string.isRequired,
     avatar: PropTypes.string.isRequired,
     online: PropTypes.bool.isRequired
-  };
+};
 export default Contact
